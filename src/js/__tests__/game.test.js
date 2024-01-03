f9d8da1b90a6ab69681e6ce0f7ecca8884994cc7
Vladimir Kalinin <Vofamchic@mail.ru>
2024-01-03 10:31:11 +0300

    modified:   src/js/__tests__/game.test.js

--- src/js/__tests__/game.test.js
+++ src/js/__tests__/game.test.js
@@ -3,12 +3,9 @@ const dataList = [
     ['critical', {name: 'Маг', health: 10}],
     ['healthy', {name: 'Маг', health: 90}],
     ['wounded', {name: 'Маг', health: 20}],
-  
   ];
   
-  const handler = test.each(dataList);
-  
-  handler('testing heal %s', (expected, params) => {
-    const result = health(params);
-    expect(result).toBe(expected);
-  })
+test.each(dataList)('testing heal %s', (expected, params) => {
+  const result = health(params);
+  expect(result).toBe(expected);
+})
